7705ea0154cb8622004633fab61f82697b803f2c
Taiki Maekawa <maekawataiki000@gmail.com>
2024-10-28 20:58:30 +0900

    Revert "v1.0: Refactor and migrate to CDK typescript"

---
+++ app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import aws_cdk as cdk
+from aws_cdk import Environment
+from aws_cdk import Aspects
+from comfyui_aws_stack.comfyui_aws_stack import ComfyUIStack
+from cdk_nag import AwsSolutionsChecks, NagSuppressions
+
+app = cdk.App()
+comfy_ui_stack = ComfyUIStack(
+    app, "ComfyUIStack",
+    description="ComfyUI on AWS (uksb-ggn3251wsp)",
+    env=Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        region=os.environ["CDK_DEFAULT_REGION"]
+    ),
+    tags={
+        "Repository": "aws-samples/cost-effective-aws-deployment-of-comfyui"
+    }
+)
+
+Aspects.of(app).add(AwsSolutionsChecks(verbose=False))
+NagSuppressions.add_stack_suppressions(stack=comfy_ui_stack, suppressions=[
+    {"id": "AwsSolutions-L1", "reason": "Lambda Runtime is provided by custom resource provider and drain ecs hook implicitely and not critical for sample"},
+    {"id": "AwsSolutions-IAM4",
+        "reason": "For sample purposes the managed policy is sufficient"},
+    {"id": "AwsSolutions-IAM5",
+        "reason": "Some rules require '*' wildcard as an example ACM operations, and other are sufficient for Sample"}
+])
+
+app.synth()
