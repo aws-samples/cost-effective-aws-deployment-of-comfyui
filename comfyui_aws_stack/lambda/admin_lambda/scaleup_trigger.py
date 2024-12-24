95b2f91e0f010b7e7e721174e6efa8dca5857e55
Taiki Maekawa <tmae@amazon.com>
2024-12-24 15:46:16 +0900

    feat: improve admin ui ux

--- comfyui_aws_stack/lambda/admin_lambda/scaleup_trigger.py
+++ comfyui_aws_stack/lambda/admin_lambda/scaleup_trigger.py
@@ -59,68 +59,16 @@ def handler(event, context):
             message = "ComfyUI is already up and running."
         else:
             message = "ComfyUI is in an unexpected state."
+        print(f"Debug: {message}")
 
     except Exception as e:
         # Handle any exceptions that occur
         print(f"Error: {e}")
         message = "Error occurred. Unable to scale ComfyUI."
 
-    html = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" /> -->
-        <title>My Website</title>
-        <style>
-            body {{
-                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                background-color: #202020;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                color: #333;
-            }}
-            main {{
-                text-align: center;
-                background-color: #ffffff;
-                padding: 40px;
-                border-radius: 5px;
-                box-shadow: 0 5px 15px rgba(0,0,0,0.2);
-                max-width: 600px;
-            }}
-            h1 {{
-                margin-bottom: 20px;
-            }}
-            p {{
-                margin-bottom: 30px;
-            }}
-            .video-link {{
-                display: inline-block;
-                background-color: #54646f;
-                color: white;
-                padding: 10px 20px;
-                text-decoration: none;
-                border-radius: 2px;
-                transition: background-color 0.3s ease;
-            }}
-            .video-link:hover {{
-                background-color: #005fa3;
-            }}
-        </style>
-    </head>
-    <body>
-        <main>
-            <h1>{message}</h1>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="video-link" target="_blank">Watch Video</a>  
-        </main>
-    </body>
-    </html>
-    """
-
-    return {"statusCode": 200, "body": html, "headers": {"Content-Type": "text/html"}}
+    return {
+        "statusCode": 302,
+        "headers": {
+            "Location": "/"
+        }
+    }
